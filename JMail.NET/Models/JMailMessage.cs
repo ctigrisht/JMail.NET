1c0e4b59a6d864f9219c038ab0f8cad3ee4cc0c9
Tigrish Chakrabarty <ctigrisht@hotmail.com>
2021-11-23 22:03:41 +0530

    Add project files.

---
+++ JMail.NET/Models/JMailMessage.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JMail.NET.Models
+{
+    public record JMailMessage
+    {
+        public JMailMessageSender Sender = null;
+        public JMailMessageReceiver Receiver = null;
+        public JMailMessageContent Content = null;
+        public DateTime Date = DateTime.MinValue;
+        /// <summary>
+        /// 0 is highest priority
+        /// </summary>
+        public short MessagePriority = 10;
+
+    }
+
+    public class JMailMessageContent
+    {
+        /// <summary>
+        /// Supports HTML format
+        /// </summary>
+        public string Message = string.Empty;
+        public string[] Files = new string[0];
+    }
+
+    public class JMailMessageSender
+    {
+        public string IPAddress = string.Empty;
+        public string Domain = string.Empty;
+        public string User = string.Empty;
+    }
+
+    public class JMailMessageReceiver
+    {
+        public string Domain = string.Empty;
+        public string User = string.Empty;
+    }
+}
